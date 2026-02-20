394d8b4d77b5ddacd062ed908b4cafd9b4bc3c0e
Mengwei Liu <mengweiliu600267@gmail.com>
2026-02-19 17:03:06 -0800

    Partitioner: support Qwen3 ops and allow delegating only first transformer layer

--- python/executorch_ggml/ggml_partitioner.py
+++ python/executorch_ggml/ggml_partitioner.py
@@ -18,26 +18,51 @@ from executorch.exir.backend.utils import tag_constant_data
 # After conversion to Edge dialect, node.target becomes an EdgeOpOverload,
 # not equal to torch.ops. So we match by name.
 _SUPPORTED_OP_NAMES = {
-    # Linear layers
+    # Qwen3 / LLM core ops
+    "aten.embedding.default",
+    "aten.linear.default",
+    "aten.silu.default",
+    "aten.mul.Tensor",
+    "aten.add.Tensor",
+    "aten.neg.default",
+    "aten.rsqrt.default",
+    "aten.mean.dim",
+    "aten.type_as.default",
+    "aten.view.default",
+    "aten.view_copy.default",
+    "aten._unsafe_view.default",
+    "aten.reshape.default",
+    "aten.unsqueeze.default",
+    "aten.unsqueeze_copy.default",
+    "aten.transpose.int",
+    "aten.slice.Tensor",
+    "aten.slice_copy.Tensor",
+    "aten.cat.default",
+    "aten.repeat_interleave.self_int",
+    "aten.index.Tensor",
+    "aten.index_put.default",
+    "aten.scaled_dot_product_attention.default",
+    "aten.alias.default",
+    "aten.alias_copy.default",
+    "aten.expand.default",
+    "aten.expand_copy.default",
+    "aten.scalar_tensor.default",
+
+    # Legacy linear demo ops
     "aten.t.default",
-    "aten.permute_copy.default",
-    "aten.addmm.default",
     "aten.mm.default",
+    "aten.addmm.default",
     "aten.leaky_relu.default",
+
     # MobileNetV2 ops
     "aten.convolution.default",
     "aten.conv2d.default",
     "aten.hardtanh.default",
     "aten.clamp.default",
-    "aten.mean.dim",
     "aten._mean_dim.default",
-    "aten.view.default",
-    "aten._unsafe_view.default",
-    "aten.reshape.default",
     "aten.permute.default",
-    "aten.add.Tensor",
+    "aten.permute_copy.default",
     "dim_order_ops._clone_dim_order.default",
-    # BatchNorm should be removed by BatchNormFoldingRewritePass prior to partitioning.
 }
 
 
@@ -49,19 +74,41 @@ BACKEND_ID = "GgmlBackend"
 
 
 class GgmlPartitioner(Partitioner):
-    """Partitions an Edge-dialect ExportedProgram, delegating supported ops to ggml."""
+    """Partitions an Edge-dialect ExportedProgram, delegating supported ops to ggml.
 
-    def __init__(self):
+    For incremental bring-up on large models (e.g. Qwen3), you can set
+    `max_sdpa_ops` to only delegate the prefix of the graph up to (but not
+    including) the (max_sdpa_ops+1)-th SDPA op.
+
+    This is a pragmatic way to "start with one layer" since transformer layers
+    contain one SDPA each.
+    """
+
+    def __init__(self, max_sdpa_ops: int | None = None):
         super().__init__()
         self.delegation_spec = DelegationSpec(BACKEND_ID, [])
+        self.max_sdpa_ops = max_sdpa_ops
 
     def partition(self, exported_program: ExportedProgram) -> PartitionResult:
         partition_tags: Dict[str, DelegationSpec] = {}
         graph_module = exported_program.graph_module
 
+        # Optionally only delegate a prefix of the graph (useful for 1-layer bring-up)
+        cutoff_idx = None
+        if self.max_sdpa_ops is not None:
+            sdpa_seen = 0
+            for idx, node in enumerate(graph_module.graph.nodes):
+                if node.op == "call_function" and "aten.scaled_dot_product_attention.default" in str(node.target):
+                    sdpa_seen += 1
+                    if sdpa_seen > self.max_sdpa_ops:
+                        cutoff_idx = idx
+                        break
+
         # Build adjacency: for each supported node, find connected supported neighbours
         supported_nodes = []
-        for node in graph_module.graph.nodes:
+        for idx, node in enumerate(graph_module.graph.nodes):
+            if cutoff_idx is not None and idx >= cutoff_idx:
+                continue
             if node.op == "call_function" and _is_supported_target(node.target):
                 supported_nodes.append(node)
 
